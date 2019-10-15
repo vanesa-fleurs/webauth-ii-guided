8a94bc15c5efc7e30e29d1c5d659cebe61ada66c
vanesa-fleurs <vanesa.fleurs@gmail.com>
2019-10-15 11:42:03 -0500

    configured express session

--- api/server.js
+++ api/server.js
@@ -1,15 +1,36 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const sessions = require('express-session')
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+const sessionConfiguration = {
+  name: 'cookiename', //default is "sid" for express- session library! 
+  secret: 'secret', //for encrypting/decrypting the cookie! (use an ENV variable)
+  cookie: {
+    httpOnly: false, // JS can't access cookies. 
+    maxAge: 60 * 60 * 1000, //experiation in milli sec.
+    //NOTE in production use HTTPS
+    secure: process.env.NODE_ENV === production ? true : false , //should be true in production only 
+  },
+  //GDPR compliance: 
+  resave: false,
+  saveUninitialized: true, //read about GDPR compliance concerning cookies
+
+};
+
+
+
+//GLOBAL MIDDLEWARE
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
+//SESSIONS
+server.use(sessions(sessionConfiguration)); //turn on sessions support 
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
@@ -19,3 +40,7 @@ server.get('/', (req, res) => {
 });
 
 module.exports = server;
+
+//session is like a table for clients
+//express-session
+//needs global middleware (so in server.js)
--- database/migrations/20180910095022_users.js
+++ database/migrations/20180910095022_users.js
@@ -6,7 +6,8 @@ exports.up = function(knex) {
       .string('username', 128)
       .notNullable()
       .unique();
-    users.string('password', 128).notNullable();
+    users.string('password', 128)
+      .notNullable();
   });
 };
 
